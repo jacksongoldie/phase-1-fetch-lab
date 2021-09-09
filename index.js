2e754893766c99c6c797e0236f4f8017e395f38e
Goldie <goldiejackson25@yahoo.com>
2021-09-09 15:21:41 -0500

    completed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  //.then(renderBooks)
+  .then(function(json){
+    return renderBooks(json)
+  })
+};
 
+debugger;
 function renderBooks(books) {
+
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
